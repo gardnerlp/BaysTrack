7dd48a0afa7ae0e9757d4ac6cb4f895480ebbe08
Shauhardha <sauharda.kshatri@gmail.com>
2025-04-02 15:50:40 -0400

    Online DB

--- database/postgresql_connection.py
+++ database/postgresql_connection.py
@@ -1,7 +1,7 @@
 import psycopg2
-from psycopg2 import sql
+import streamlit as st
 
-def init_postgres_connection():
+def init_postgres_connection_local():
     """
     Initializes a connection to the PostgreSQL database.
     """
@@ -17,3 +17,20 @@ def init_postgres_connection():
         print("Error connecting to the PostgreSQL database:", e)
         raise
 
+
+def init_postgres_connection():
+    """
+    Initializes a connection to the PostgreSQL database.
+    """
+    try:
+        conn = psycopg2.connect(
+            host=st.secrets["host"],  # Replace with your PostgreSQL host
+            port=st.secrets["port"],
+            database=st.secrets["database"],  # Replace with your PostgreSQL database name
+            user=st.secrets["user"],  # Replace with your PostgreSQL username
+            password= st.secrets["auth_key"]
+        )
+        return conn
+    except Exception as e:
+        print("Error connecting to the PostgreSQL database:", e)
+        raise
