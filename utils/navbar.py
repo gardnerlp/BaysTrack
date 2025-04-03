50b74f61c8023199ba43e4ed471138a6ce77a3c2
Shauhardha <sauharda.kshatri@gmail.com>
2025-04-03 01:38:13 -0400

    Changes in navbar

--- utils/navbar.py
+++ utils/navbar.py
@@ -7,12 +7,12 @@ def navbar():
 
     st.sidebar.markdown(f"<p style='font-size:25px; font-weight:bold; font-family:'Calibri';'>{username}</p>",unsafe_allow_html=True)
     st.sidebar.page_link('app.py', label='Home')
-    st.sidebar.page_link('pages/Notes.py', label='Notes')
-    st.sidebar.page_link('pages/Calendar.py', label='Calendar')
-    st.sidebar.page_link('pages/Ethogram_Form.py', label='Ethogram Form')
+    st.sidebar.page_link('pages/notes.py', label='Notes')
+    st.sidebar.page_link('pages/calendar.py', label='Calendar')
+    st.sidebar.page_link('pages/ethogram_form.py', label='Ethogram Form')
     if "role" in st.session_state and st.session_state["role"] == "admin":
         st.sidebar.page_link('pages/Admin.py', label='Admin')
-        st.sidebar.page_link('pages/Habitat.py', label='Animal & Habitat')
+        st.sidebar.page_link('pages/habitat.py', label='Animal & Habitat')
     else:
         st.sidebar.page_link('pages/Admin.py', label='User')
     
